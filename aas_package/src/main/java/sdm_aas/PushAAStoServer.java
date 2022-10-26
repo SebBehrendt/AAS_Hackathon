80aa518387a3a9e5091454161cb4952af966ab00
SebBehrendtC:/Users/Sebas/OneDrive/Documents/venvs/simpy_venv/Scripts/python.exe <sebastian.behrendt@kit.edu>
2022-10-26 15:00:14 +0200

    Added push method

---
+++ aas_package/src/main/java/sdm_aas/PushAAStoServer.java
@@ -0,0 +1,37 @@
+package sdm_aas;
+
+import org.eclipse.basyx.aas.manager.ConnectedAssetAdministrationShellManager;
+import org.eclipse.basyx.aas.metamodel.map.AssetAdministrationShell;
+import org.eclipse.basyx.aas.registration.proxy.AASRegistryProxy;
+import org.eclipse.basyx.submodel.metamodel.map.Submodel;
+import java.util.List;
+import java.util.ArrayList;
+
+public class PushAAStoServer {
+	
+    /**
+     * Pushes the AAS to a server and registers it
+     * 
+     * @param aas the AssetAdministrationShell to be pushed to the server
+     * @param aasServerURL the URL of the aas server (e.g. http://localhost:8080/aasComponent)
+     * @param registryServerURL the URL of the registry server (e.g. http://localhost:8080/registry)
+     */
+    public static void pushAAS(AssetAdministrationShell aas, String aasServerURL, String registryServerURL, List<Submodel> listOfSM) {
+        // Create a proxy pointing to the registry server
+        AASRegistryProxy registryProxy = new AASRegistryProxy(registryServerURL);
+        
+        // Create a ConnectedAASManager using the registryProxy as its registry
+        ConnectedAssetAdministrationShellManager manager =
+                new ConnectedAssetAdministrationShellManager(registryProxy);
+        
+        // The ConnectedAASManager automatically pushes the given AAS
+        // to the server to which the address was given
+        // It also registers the AAS in the registry it got in its ctor
+        manager.createAAS(aas, aasServerURL);
+        for (Submodel sm : listOfSM)
+        {
+            manager.createSubmodel(aas.getIdentification(), sm);
+        }
+        
+       }
+}
